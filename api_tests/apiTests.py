d186c0d23965717c964679097e9d183c0780426f
Hrishabh598 <hrishabhtiwari598@gmail.com>
2025-01-26 19:49:06 +0530

    added parametrized tests using pytest.mark.parametrize

--- api_tests/apiTests.py
+++ api_tests/apiTests.py
@@ -1,16 +1,18 @@
 import requests
+import pytest
 
-def test_check_api():
-    url = "https://jsonplaceholder.typicode.com/users/1"
+@pytest.mark.parametrize("userid,status_code,header,name,bs_element",[(1,200,"application/json; charset=utf-8","Leanne Graham","harness real-time e-markets"),(2,200,"application/json; charset=utf-8","Ervin Howell","synergize scalable supply-chains"),(3,200,"application/json; charset=utf-8","Clementine Bauch","e-enable strategic applications")])
+def test_check_api(userid,status_code,header,name,bs_element):
+    url = f"https://jsonplaceholder.typicode.com/users/{userid}"
     response = requests.get(url)
 
-    assert response.status_code==200 ,f"Expected response status code 200 but got {response.status_code}"
+    assert response.status_code==status_code ,f"Expected response status code 200 but got {response.status_code}"
 
-    assert response.headers["Content-Type"]=="application/json; charset=utf-8" ,f"got content-type {response.headers['Content-Type']}"
+    assert response.headers["Content-Type"]==header ,f"got content-type {response.headers['Content-Type']}"
 
     json_response = response.json()
 
-    assert json_response["name"]=="Leanne Graham", f"expected the name to be Leane Graham but got {json_response['name']}"
+    assert json_response["name"]==name, f"expected the name to be {name} but got {json_response['name']}"
 
-    assert json_response['company']['bs'] == 'harness real-time e-markets', f"expected company.bs 'harness real-time e-markets' but got {json_response['company']['bs']}"
+    assert json_response['company']['bs'] == bs_element, f"expected company.bs '{bs_element}' but got {json_response['company']['bs']}"
 
